c4493db4667ee4b0c365e78c734a5ede9ddbd853
Juan Cruz <jc23729@gmail.com>
2021-02-14 12:56:01 -0500

    express error handling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+const ExpressError = require('./expressError');
 
 app.get('/', function(req, res){
     res.send('Hello World');
---
+++ expressError.js
@@ -0,0 +1,5 @@
+/** ExpressError extends the normal JS error so we can easily
+ *  add a status when we make an instance of it.
+ *
+ *  The error-handling middleware will return this.
+ */
