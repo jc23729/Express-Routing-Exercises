cb691fc64b1eac46c22d1a573ad71a26d3e5954d
Juan Cruz <jc23729@gmail.com>
2021-02-14 13:32:38 -0500

    helpers test

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const express = require('express');
-
-const app = express();
-const ExpressError = require('./expressError');
-
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
 // Express Routing Exercises
 // For this exercise, you will build an Express.js application that performs three statistical operations given an arbitrary amount of numbers:
 
@@ -31,10 +22,100 @@ app.get('/', function(req, res){
 //spin up server    nodemon app.js in commandline / terminal
 // go to localhost:3000 on browser to see your server and code
 
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
 const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require('./helpers');
 
-app.listen(3000, function () {
-  console.log('App on port 3000');
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/mean', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "mean",
+    result: findMean(nums)
+  }
+
+  return res.send(result);
+});
+
+app.get('/median', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums)
+  }
+
+  return res.send(result);
+  
+});
+
+app.get('/mode', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums)
+  }
+
+  return res.send(result);
+
+ 
+});
+
+/** general error handler */
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found",404);
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log(`Server starting on port 3000`);
 });
 
 
